59f065fc607ca587504a43683d680ee4e1386323
joiky <joiky.g@163.com>
2023-01-24 11:49:20 +0800

    add goconvey test

--- pkg/expand/expand_test.go
+++ pkg/expand/expand_test.go
@@ -4,20 +4,33 @@ import (
 	"testing"
 
 	"github.com/itozll/iskep/pkg/expand"
+	"github.com/smartystreets/goconvey/convey"
 )
 
-var test_elements = []string{
-	"hello_http_v1",
-	"hello_httpv1",
-	"HELLOHttpV1",
-	"HTTPhelloV123",
-	"HTTPhellop123",
-	"Ab!@#!@$!@$#&word",
-	"",
+var test_elements = []struct {
+	str    string
+	pascal string
+	camel  string
+	snake  string
+	kebad  string
+}{
+	{"hello_http_v1", "HelloHTTPV1", "helloHTTPV1", "hello_http_v1", "hello-hello-v1"},
+	{"hello_httpv1", "HelloHTTPV1", "helloHTTPV1", "hello_http_v1", "hello-hello-v1"},
+	{"HTTPhelloV123", "HTTPHelloV123", "httpHelloV123", "http_hello_v123", "http-hello-v123"},
+	{"HTTPhellop123", "HTTPHellop123", "httpHellop123", "http_hellop_123", "http-hellop-123"},
+	{"HTTP###@hellop123", "HTTPHellop123", "httpHellop123", "http_hellop_123", "http-hellop-123"},
+	{"HTTP hellop123", "HTTPHellop123", "httpHellop123", "http_hellop_123", "http-hellop-123"},
 }
 
 func TestExpand(t *testing.T) {
-	for _, elem := range test_elements {
-		t.Log(elem, expand.Pascal(elem), expand.Snake(elem), expand.Camel(elem))
-	}
+	convey.Convey("Test expand", t, func() {
+		for _, elem := range test_elements {
+			convey.Convey("Test `"+elem.str+"'", func() {
+				convey.ShouldEqual(expand.Pascal(elem.str), elem.pascal)
+				convey.ShouldEqual(expand.Camel(elem.str), elem.camel)
+				convey.ShouldEqual(expand.Snake(elem.str), elem.snake)
+				convey.ShouldEqual(expand.Kebad(elem.str), elem.kebad)
+			})
+		}
+	})
 }
